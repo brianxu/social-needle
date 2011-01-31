c00a81abf448329a2e522ac050c8cff6b15c59c0
garthcn <garthcn@gmail.com>
2011-01-30 16:18:37 -0800

    done with updating users. missing others

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
     else
       # Sign in and redirect to the show page
       sign_in user
-      redirect_to user
+      redirect_back_or user
     end
 
   end

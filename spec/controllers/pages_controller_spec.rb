0a92237f0e3480541fd5b3a9c3883f41250570b7
garthcn <garthcn@gmail.com>
2011-02-02 22:46:41 -0800

    ui tweak

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -11,7 +11,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector("title", :content => "Social Needle | Home")
     end
   end
 
@@ -23,7 +23,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title", :content => "Social Needle | Contact")
     end
 
   end
@@ -35,7 +35,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title", :content => "Social Needle | About")
     end
   end
 end

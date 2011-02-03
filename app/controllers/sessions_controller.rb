94c09b09e27cad6fab340bacf84755c505a2bb68
garthcn <garthcn@gmail.com>
2011-02-02 23:28:17 -0800

    tweak

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
     else
       # Sign in and redirect to the show page
       sign_in user
-      redirect_back_or user
+      redirect_back_or root_path
     end
 
   end
